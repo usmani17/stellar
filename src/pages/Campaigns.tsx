55d94160db085285f30fd1315f769d832f9a9f60
i-zee13 <100059891+i-zee13@users.noreply.github.com>
2026-02-02 18:52:53 +0500

    fix the pagination issue

--- src/pages/Campaigns.tsx
+++ src/pages/Campaigns.tsx
@@ -4154,7 +4154,7 @@ export const Campaigns: React.FC = () => {
                         ...
                       </span>
                     )}
-                    {totalPages > 5 && (
+                    {totalPages > 5 && currentPage < totalPages - 2 && (
                       <button
                         onClick={() => handlePageChange(totalPages)}
                         className={`px-3 py-2 border-r border-gray-200 text-[10.64px] cursor-pointer ${currentPage === totalPages
