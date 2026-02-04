e2e5c45ece8605494c9ce70ec402a1b2d1f6994d
Rashid Ali <rashid.ali000@gmail.com>
2026-02-04 13:12:38 +0500

    archivie campaign issue

--- src/components/layout/DashboardHeader.tsx
+++ src/components/layout/DashboardHeader.tsx
@@ -16,6 +16,7 @@ import { type Account, accountsService } from "../../services/accounts";
 import CustomDateRangePicker from "../ui/CustomDateRangePicker";
 import GoogleIcon from "../../assets/images/ri_google-fill.svg";
 import AmazonIcon from "../../assets/images/amazon-fill.svg";
+import MetaIcon from "../../assets/images/mingcute_meta-line.svg";
 
 // Generate a color based on the first letter of the brand name
 const getInitialColor = (initial: string): string => {
@@ -105,6 +106,13 @@ const AccountChannelsList: React.FC<{
                   >
                     <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z" />
                   </svg>
+                )}
+                {channel.channel_type === "meta" && (
+                  <img
+                    src={MetaIcon}
+                    alt="Meta"
+                    className="w-4 h-4 flex-shrink-0"
+                  />
                 )}
                 <span>{channel.channel_name}</span>
               </button>
@@ -349,6 +357,9 @@ export const DashboardHeader: React.FC = () => {
                       >
                         <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z" />
                       </svg>
+                    )}
+                    {selectedChannel.channel_type === "meta" && (
+                      <img src={MetaIcon} alt="Meta" className="w-4 h-4" />
                     )}
                     <span className="text-[13.2px] text-[#556179]">
                       {selectedChannel.channel_name}
